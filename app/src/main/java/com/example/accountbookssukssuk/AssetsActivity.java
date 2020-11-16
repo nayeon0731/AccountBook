59a45a15d88ac574605481171551240cd14f9bb6
HYOBIN <52150400+lottt105@users.noreply.github.com>
2020-11-16 16:49:23 +0900

    back button
    뒤로가기 버튼(입력창)

--- app/src/main/java/com/example/accountbookssukssuk/AssetsActivity.java
+++ app/src/main/java/com/example/accountbookssukssuk/AssetsActivity.java
@@ -1,16 +1,19 @@
 package com.example.accountbookssukssuk;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toolbar;
 
 import com.example.accountbookssukssuk.total.TotalAdapter;
 import com.example.accountbookssukssuk.total.TotalDB;
@@ -31,10 +34,17 @@ public class AssetsActivity extends AppCompatActivity {
     TotalDB total_database;
     TotalAdapter total_adapter;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assets);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //툴바 뒤로가기 버튼
+
 
         // Assign variable
         mainCategory = findViewById(R.id.main_category_text);
@@ -86,5 +96,16 @@ public class AssetsActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    //툴바 뒤로가기 실행
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item ){
+        switch(item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/accountbookssukssuk/InsertActivity.java
+++ app/src/main/java/com/example/accountbookssukssuk/InsertActivity.java
@@ -1,9 +1,11 @@
 package com.example.accountbookssukssuk;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -18,6 +20,11 @@ public class InsertActivity extends AppCompatActivity {
 
         btn_import = (Button)findViewById(R.id.btn_import);
         btn_export = (Button)findViewById(R.id.btn_export);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //툴바 뒤로가기 버튼
 
         btn_import.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,5 +45,16 @@ public class InsertActivity extends AppCompatActivity {
                 transaction.commit();
             }
         });
+    }
+
+    //툴바 뒤로가기 실행
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item ){
+        switch(item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
