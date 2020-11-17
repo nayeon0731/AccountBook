104790e4dba72169e59964a2e74c352f30262d27
Mi-Gyeong <rmfosem613@gmail.com>
2020-11-17 13:15:20 +0900

    Add Spinner in Import.xml

--- app/src/main/java/com/example/accountbookssukssuk/ImportActivity.java
+++ app/src/main/java/com/example/accountbookssukssuk/ImportActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.graphics.Color;
@@ -13,19 +14,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class ImportActivity extends Fragment {
     public ImportActivity(){
-
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_import, container, false);
+        ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.activity_import, container, false);
+        Spinner object_spinner = viewGroup.findViewById(R.id.object_category_text);
+        final TextView array_text = viewGroup.findViewById(R.id.array_income_text);
+
+        object_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                array_text.setText(""+parent.getItemAtPosition(position));
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        return viewGroup;
     }
 }
