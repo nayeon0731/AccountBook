4f00c60c96b66111b19c1bf8ccdabf61520eec79
Mi-Gyeong <rmfosem613@gmail.com>
2020-11-17 21:20:47 +0900

    Add DatePicker in ImportActivity

--- app/src/main/java/com/example/accountbookssukssuk/ImportActivity.java
+++ app/src/main/java/com/example/accountbookssukssuk/ImportActivity.java
@@ -5,30 +5,49 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class ImportActivity extends Fragment {
+
     public ImportActivity(){
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_import, container, false);
+        ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.activity_import, container, false);
 
+        TextView date_text = viewGroup.findViewById(R.id.income_date_text);
+        Button income_date_picker = viewGroup.findViewById(R.id.date_picker);
+
+        income_date_picker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new SelectDateFragment();
+                newFragment.show(getFragmentManager(), "DatePicker");
+            }
+        });
+
+        return viewGroup;
     }
+
+
 }
---
+++ app/src/main/java/com/example/accountbookssukssuk/SelectDateFragment.java
@@ -0,0 +1,34 @@
+package com.example.accountbookssukssuk;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class SelectDateFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar calendar = Calendar.getInstance();
+        int yy = calendar.get(Calendar.YEAR);
+        int mm = calendar.get(Calendar.MONTH);
+        int dd = calendar.get(Calendar.DAY_OF_MONTH);
+        return new DatePickerDialog(getActivity(), this, yy, mm, dd);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        populateSetDate(year, month+1,dayOfMonth);
+    }
+
+    public void populateSetDate(int year, int month, int day) {
+        TextView income_date_text= (TextView)getActivity().findViewById(R.id.income_date_text);
+        income_date_text.setText(year+"/"+month+"/"+day);
+    }
+}
